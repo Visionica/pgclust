0306da0d04b8420e952406cdc879079d65158660
Andrey Tolstoy <avtolstoy@gmail.com>
2013-02-27 03:37:58 +0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pgclust',
+    version='1.0',
+    description='Stupid postgres cluster manager',
+    author='Andrey Tolstoy',
+    author_email='avtolstoy@gmail.com',
+    url='http://tolstoy.it',
+    packages=['pgclust'],
+    data_files=[('/usr/sbin', 'pgclust')]
+    )
